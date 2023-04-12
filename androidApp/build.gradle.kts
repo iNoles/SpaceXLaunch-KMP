c3f56c54f78348d0d933a26576f4b2b110d08916
Jonathan Steele <iNoles@users.noreply.github.com>
2023-04-12 16:58:50 -0400

    Upgrade to Kotlin 1.8.20

--- androidApp/build.gradle.kts
+++ androidApp/build.gradle.kts
@@ -20,7 +20,7 @@ android {
         compose = true
     }
     composeOptions {
-        kotlinCompilerExtensionVersion = "1.4.4"
+        kotlinCompilerExtensionVersion = "1.4.5"
     }
     packaging {
         resources {
